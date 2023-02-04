59344d1c4bce67e98ea5b1cb89e1be2fdc837d78
sans <clafernalo@gmail.com>
2023-02-04 15:43:07 +0800

    blog

---
+++ src/components/Blog/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Blog(props) {
+    return (
+        <div class="alert alert-primary container-sm w-100" role="alert">
+        <img src="https://em-content.zobj.net/thumbs/120/apple/325/moai_1f5ff.png" className="bi flex-shrink-0 me-2 emoji" />
+            This blog section is under construction (currently working on the backend).
+        </div>
+    );
+}
