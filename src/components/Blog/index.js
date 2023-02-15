ecbe6909ad3154641269d657f090d5d5f1e8cc94
sans <clafernalo@gmail.com>
2023-02-16 03:53:13 +0800

    fixes blog page

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -2,9 +2,13 @@ import * as React from 'react';
 
 export default function Blog(props) {
     return (
-        <div class="alert alert-primary container-sm w-100" role="alert">
-        <img src="emoji_moyai.png" className="bi flex-shrink-0 me-2 emoji" alt="moyai emoji"/>
-            This blog section is under construction (currently working on the backend).
+        <div className="w-100 d-flex flex-column align-items-center">
+            <div className="main-body">
+                <div class="alert alert-primary container-sm w-100" role="alert">
+                    <img src="emoji_moyai.png" className="bi flex-shrink-0 me-2 emoji" alt="moyai emoji"/>
+                    This blog section is under construction (currently working on the backend).
+                </div>
+            </div>
         </div>
     );
 }
